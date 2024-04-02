efccd2255dea6812d5f3157850c54f331335c670
MilosN <miloskm@live.com>
2024-04-02 20:37:06 +0200

    add vueSPA support

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'app', 'dist')));
+
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, 'app', 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
