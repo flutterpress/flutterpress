be49b492f5dad97d312011e438ebb045179c7970
MilosN <miloskm@live.com>
2024-04-02 20:44:23 +0200

    add hot reload

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'app', 'dist')));
 
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: 'This is your backend response 2' });
+});
+
 app.get('*', (req: Request, res: Response) => {
     res.sendFile(path.resolve(__dirname, 'app', 'dist', 'index.html'));
 });
